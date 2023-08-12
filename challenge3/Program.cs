3189e0fb8dec69a9181055cf1b2ed1148e44ff62
Riley Potts <riley.potts18@gmail.com>
2023-08-12 13:07:27 +0100

    first commit

---
+++ challenge3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace challenge3{
+    class Program{
+        static void FindMinMax(int[] num){
+            Array.Sort(num);
+            foreach(var item in num) {
+                Console.Write(item.ToString() + " "); 
+            }
+        }
+
+        static void Main(string[] args){
+            int[] num = new int[] { 3, 1, 2};
+            FindMinMax(num);
+        }
+    }
+}
